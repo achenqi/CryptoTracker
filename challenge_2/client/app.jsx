051a8e98ad9e487ab7da5d2cd3a4b82f4ac0067a
Aowei Chenqi <aoweichenqi@Aoweis-MacBook-Pro.local>
2021-02-16 19:44:00 -0800

    BMR Complete

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,39 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactChart from './components/chart.jsx';
-import Chart from "chart.js";
+import Chart from 'chart.js';
+import axios from 'axios';
 
 class Index extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      ctx: undefined
+      ctx: undefined,
+      data: undefined,
+      labels: []
     }
+    this.apiRequest = this.apiRequest.bind(this);
   }
 
-  componentDidMount() {
-    this.setState({
-      ctx: document.getElementById.bind(document)('myChart').getContext('2d')
-    }, () => {
-        new Chart(this.state.ctx, {
-          // The type of chart we want to create
-          type: 'line',
+  apiRequest() {
+    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+      .then(data => {
+        this.setState({
+            data: data, labels: Object.keys(data.data.bpi), ctx: document.getElementById.bind(document)('myChart').getContext('2d')
+          }, () => {
+            new Chart(this.state.ctx, {
+              // The type of chart we want to create
+              type: 'line',
 
-          // The data for our dataset
-          data: {
-              labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-              datasets: [{
-                  label: 'My First dataset',
-                  backgroundColor: 'rgb(255, 99, 132)',
-                  borderColor: 'rgb(255, 99, 132)',
-                  data: [0, 10, 5, 2, 20, 30, 45]
-              }]
-          },
+              // The data for our dataset
+              data: {
+                  labels: this.state.labels,
+                  datasets: [{
+                      label: 'BTC Price',
+                      backgroundColor: 'rgb(255, 99, 132)',
+                      borderColor: 'rgb(255, 99, 132)',
+                      data: Object.values(data.data.bpi)
+                  }]
+              },
 
-          // Configuration options go here
-          options: {}
+              // Configuration options go here
+              options: {}
+          })
+        },()=> {console.log(this.state)});
       })
-    })
+      .catch((err) => console.log(err))
+  }
+  componentDidMount() {
+    this.apiRequest();
   }
    render() {
 
