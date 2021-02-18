b7b47e691b0f7500a0a1ba5415346284b153b0c4
Aowei Chenqi <aoweichenqi@Aoweis-MacBook-Pro.local>
2021-02-17 20:29:00 -0800

    Recreated with Hooks

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import ReactChart from './components/chart.jsx';
 import Chart from 'chart.js';
 import axios from 'axios';
 
-class Index extends React.Component {
+function Index() {
+  const[labels, setLabels] = useState([]);
+  const[data, setData] = useState();
+  const[ctx, setCtx] = useState();
+  const[init, setInit] = useState(false);
+
+  useEffect(()=> {
+    if (init === false) {
+      apiRequest();
+      setInit(true);
+    }
+  }, [init])
+
+  function apiRequest() {
+    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+      .then(data => {
+        setLabels(Object.keys(data.data.bpi));
+        setData(data);
+        setCtx(document.getElementById('myChart').getContext('2d'))
+      })
+      .catch((err) => console.log(err))
+  }
+
+  useEffect(()=> {
+    if (data !== undefined && labels.length > 0 && ctx !== undefined) {
+      (() => {
+        new Chart(ctx, {
+          // The type of chart we want to create
+          type: 'line',
+
+          // The data for our dataset
+          data: {
+              labels:labels,
+              datasets: [{
+                  label: 'BTC Price',
+                  backgroundColor: 'rgb(255, 99, 132)',
+                  borderColor: 'rgb(255, 99, 132)',
+                  data: Object.values(data.data.bpi)
+              }]
+          },
+
+          // Configuration options go here
+          options: {}
+        })
+      })();
+    }
+  },[data,labels, ctx])
+
+  return (
+    <div>
+      <h1>BTC Price History</h1>
+      <ReactChart/>
+    </div>
+    )
+
+}
+
+ReactDOM.render(<Index/>,document.getElementById('root'))
+
+/*class Index extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -58,4 +117,4 @@ class Index extends React.Component {
 }
 
 
-ReactDOM.render(<Index/>,document.getElementById('root'))
+ReactDOM.render(<Index/>,document.getElementById('root'))*/
