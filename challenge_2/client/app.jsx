f18bcdd8956edc8132222a4aaa97d30be03c1c88
Aowei Chenqi <aoweichenqi@Aoweis-MacBook-Pro.local>
2021-02-18 20:03:20 -0800

    igore check

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -8,53 +8,73 @@ function Index() {
   const[labels, setLabels] = useState([]);
   const[data, setData] = useState();
   const[ctx, setCtx] = useState();
-  const[init, setInit] = useState(false);
+  const[localOnly, setLocal] = useState(true);
+  const storage = window.localStorage;
 
   useEffect(()=> {
-    if (init === false) {
-      apiRequest();
-      setInit(true);
+      setCtx(document.getElementById('myChart').getContext('2d'));
+  }, [])
+
+  useEffect(() => {
+    if (localOnly === true) {
+      if (storage.getItem('cache')) {
+        const localInfo = JSON.parse(storage.getItem('cache'));
+        setLabels(localInfo.labels);
+        setData(localInfo.data);
+      }
     }
-  }, [init])
+    if (localOnly === false) {
+      apiRequest();
+    }
+  }, [localOnly]);
 
   function apiRequest() {
     axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
       .then(data => {
         setLabels(Object.keys(data.data.bpi));
         setData(data);
-        setCtx(document.getElementById('myChart').getContext('2d'))
+        storage.setItem('cache', JSON.stringify({labels: Object.keys(data.data.bpi), data: data }));
       })
       .catch((err) => console.log(err))
   }
 
+  function onCacheClick() {
+    setLocal(!localOnly);
+  }
+
+  function renderChart() {
+    var chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'line',
+
+      // The data for our dataset
+      data: {
+          labels:labels,
+          datasets: [{
+              label: 'BTC Price',
+              backgroundColor: 'rgb(242, 169, 0)',
+              borderColor: 'rgb(242, 169, 0)',
+              data: Object.values(data.data.bpi),
+              pointBackgroundColor:'rgb(77, 77, 78)',
+              pointBorderColor:'rgb(0, 0, 0)'
+          },]
+      },
+
+      // Configuration options go here
+      options: {}
+    })
+  }
+
   useEffect(()=> {
     if (data !== undefined && labels.length > 0 && ctx !== undefined) {
-      (() => {
-        new Chart(ctx, {
-          // The type of chart we want to create
-          type: 'line',
-
-          // The data for our dataset
-          data: {
-              labels:labels,
-              datasets: [{
-                  label: 'BTC Price',
-                  backgroundColor: 'rgb(255, 99, 132)',
-                  borderColor: 'rgb(255, 99, 132)',
-                  data: Object.values(data.data.bpi)
-              }]
-          },
-
-          // Configuration options go here
-          options: {}
-        })
-      })();
+      renderChart();
     }
   },[data,labels, ctx])
 
   return (
     <div>
-      <h1>BTC Price History</h1>
+      <img src='bitcoin-logo-dark-300x63.png' className='logo'></img>
+      <a className="cacheButton" onClick = {onCacheClick}>{localOnly ? "Search new info" : "Use Cache"}</a>
       <ReactChart/>
     </div>
     )
