6bbfb61ef03910fb0fc7a6996ff31a064fc4fd06
achenqi <31229294+achenqi@users.noreply.github.com>
2021-04-12 20:38:56 -0700

    Delete challenge_3 directory

--- challenge_3/client/components/pins.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Pins (props) {
-  return (
-  <th onClick={() => props.pinsHit(props.number)}>{props.number}</th>
-  )
-}
-
-export default Pins;
--- challenge_3/client/components/score.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Score(props){
-  return (
-    props.score.map((turn, key) => <div className='score-turn' key={key}>{turn[1]} | {turn[2]} {(turn[1] + turn[2] === 10) ? '| SPARE' : ''}</div>)
-  )
-}
-
-export default Score;
